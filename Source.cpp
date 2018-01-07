d4bf492a1c1b3690a1be30dccc30e2b274771553
Dathr <Dathr@users.noreply.github.com>
2018-01-07 09:50:39 -0800

    Added Functions, fixed loops
    Added puzzle struct functions:
    bool isSolved(with implementation), void gameplay(without contents)
    
    Renamed puzzle struct function:
    void setup is now named void setItUp
    
    added comments
    
    implemented dataFile.close after the while loop now starting on line 77.

--- Source.cpp
+++ Source.cpp
@@ -10,6 +10,7 @@ int selection;
 int temp1;													//for holding row number for selection and changing
 int temp2;													//for holding column number for selection and changing
 string buf;													//holds a line of data from the file
+string entries;												//holds user entries for the puzzle
 ifstream dataFile;											//used to open the file(select a puzzle)
 
 struct puzzle
@@ -28,7 +29,7 @@ struct puzzle
 	{
 		gridState[x-1][y-1] = false;
 	};
-	void setup()											//sets up a clear starting grid. Needs to create a solution as well.
+	void setItUp()											//sets up a clear starting grid. Needs to create a solution as well.
 	{
 		for (int i = 0; i < 4; i++)
 		{
@@ -38,6 +39,19 @@ struct puzzle
 	};
 	bool isSolved()
 	{
+		for(int i=0; i<4; i++)
+		{
+			for(int j=0; j<4; j++)
+			{
+				if(solution[i][j]!=gridState[i][j])
+				return false;
+			};	
+		};
+		return true;
+	};
+	void gameplay(string x)
+	{
+		//NOT DESIGNED YET. PLEASE FILL IN LATER.
 	};
 
 };
@@ -48,23 +62,26 @@ int main()
 {
 	puzzle game;
 	cout << "Please select a number between 1 and 1: ";		//picking a puzzle. Only 1 exists so far.
-	cin >> selection;
+	cin >> selection;										//the int selection of a puzzle
 	cout << endl << "Okay! Here goes..." << endl;
 	if (selection == 1)										//a placeholder for a better puzzle selection system.
 		dataFile.open("samplePuzzle.txt");
 	else													//for smart alecs who didn't select 1.
 	{
 		cout << "no. nonono. It failed. Sorry, eh?" << endl << "press something and then enter to leave. sorry again. ";
-		cin >> messedUp;
+		cin >> messedUp;									//prompt so the user can read the above text before the program terminates
 		return 0;
 	};											
-	dataFile.open("samplePuzzle.txt");
-	while(dataFile.good())
+	dataFile.open("samplePuzzle.txt");						//open the file
+	
+	while(dataFile.good())									//loop to read file and create solution grid
 	{
 		getline(dataFile, buf);
 		temp1=atoi(buf.c_str[0]);
 		temp2=atoi(buf.c_str[1]);
 		game.setSol(temp1, temp2);
-	}
+	};
+	dataFile.close();										//close the file
+
 	return 0;
 }
