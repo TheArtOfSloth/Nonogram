d58bb557a7de7c6b9becdd270fc6a0a09daf80dd
Dathr <Dathr@users.noreply.github.com>
2018-01-09 13:32:10 -0800

    classes added
    Implemented basic clas structure format for previous struct puzzle. needs cleanup.

--- Source.cpp
+++ Source.cpp
@@ -7,28 +7,38 @@ using namespace std;
 
 string messedUp;
 int selection;
-int temp1;													//for holding row number for selection and changing
-int temp2;													//for holding column number for selection and changing
-string buf;													//holds a line of data from the file
-string entries;												//holds user entries for the puzzle
-ifstream dataFile;											//used to open the file(select a puzzle)
+int temp1;														//for holding row number for selection and changing
+int temp2;														//for holding column number for selection and changing
+string buf;														//holds a line of data from the file
+string entries;													//holds user entries for the puzzle
+ifstream dataFile;												//used to open the file(select a puzzle)
 
-/*
 
 class puzzle()
 {
-	ifstream dataFil;
-	string buff;
-	int temp3;
-	int temp4;
-	bool solution[4][4];
+	ifstream dataFil;											//for opening a file
+	string buff;												//for reading the file
+	int temp3;													//for input into setSol
+	int temp4;													//for input into setSol
+	bool solution[4][4];										//solution grid
+	void clearSol()												//for clearing the solution before setting it up
+	{
+		for (int i=0; i<4; i++)
+		{
+			for (int j=0; j<4; j++)
+			{
+				solution[i][j]=false;
+			};
+		};
+	};
 	void setSol(int x, int y)									//for setting up the solution
 	{
 		solution[x-1][y-1] = true;
 	};
-	puzzle (string w)
+	puzzle(string w)
 	{
-			dataFil.open(w);									//open the file
+		clearSol();
+		dataFil.open(w);										//open the file
 		while(dataFil.good())									//loop to read file and create solution grid
 		{
 			getline(dataFil, buff);
@@ -41,9 +51,34 @@ class puzzle()
 
 };
 
-*/
 
-struct puzzle
+class game()
+{
+private:
+	bool gridStat[4][4];
+public:
+	game()
+	{
+		for (int i=0; i<4; i++)
+		{
+			for (int j=0; j<4; j++)
+			{
+				gridStat[i][j]=false;
+			};
+		};
+	};
+	void changeSpace(int x, int y)
+	{
+		if(gridStat[x-1][y-1]==true)
+			gridStat[x-1][y-1]=false;
+		else
+			gridStat[x-1][y-1]=true;
+	};
+};
+
+
+
+struct puzzl
 {
 	bool solution[4][4];									//the solution grid. This should only change when first created. Or if a new puzzle starts.
 	bool gridState[4][4];									//the player grid. This should hold the data for the current game. A graphical depiction should be built.
@@ -51,7 +86,7 @@ struct puzzle
 	{
 		solution[x-1][y-1] = true;
 	};
-	void setGrid(int x, int y)							//for changing values to true
+	void setGrid(int x, int y)								//for changing values to true
 	{
 		gridState[x-1][y-1] = true;
 	};
@@ -90,7 +125,7 @@ struct puzzle
 
 int main()
 {
-	puzzle game;
+	puzzl gam;
 	cout << "Please select a number between 1 and 1: ";		//picking a puzzle. Only 1 exists so far.
 	cin >> selection;										//the int selection of a puzzle
 	cout << endl << "Okay! Here goes..." << endl;
@@ -109,7 +144,7 @@ int main()
 		getline(dataFile, buf);
 		temp1=atoi(buf.c_str[0]);
 		temp2=atoi(buf.c_str[1]);
-		game.setSol(temp1, temp2);
+		gam.setSol(temp1, temp2);
 	};
 	dataFile.close();										//close the file
 
